b4c39a665a2b89d42d5e9a10a4c5c7ce73dbe4cc
Hack998 <38637984+Hack998@users.noreply.github.com>
2018-05-06 17:22:32 -0600

    Sockets
    Se creo la logica para conectar con el cliente

--- src/Sockets.java
+++ src/Sockets.java
@@ -1,4 +1,25 @@
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Sockets {
-
+	public static void conectar() throws Exception {
+		ServerSocket serversocket = new ServerSocket(113, 10);
+		System.out.println("Listo");
+		while (true) {
+			Socket client = serversocket.accept();
+			Scanner scanner = new Scanner(client.getInputStream());
+			PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
+			String name = scanner.nextLine();
+			;
+			if ("hola".equals(name)) {
+				String xml = "<good>Found</good>";
+				pw.println(xml);
+			} else {
+				pw.println("<error>Not found</error>");
+			}
+			client.close();
+		}
+	}
 }
