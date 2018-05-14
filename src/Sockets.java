270b0947e545918c8176197a029055d31a699824
Hack998 <38637984+Hack998@users.noreply.github.com>
2018-05-13 20:37:41 -0600

    Creacion de Usuarios
    Implementacion de la creacion de usuarios por medio de Sockets

--- src/Sockets.java
+++ src/Sockets.java
@@ -2,6 +2,8 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import javax.json.JsonObject;
+import users.NewUser;
 
 public class Sockets {
 	public static void conectar() throws Exception {
@@ -12,12 +14,75 @@ public class Sockets {
 			Scanner scanner = new Scanner(client.getInputStream());
 			PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
 			String name = scanner.nextLine();
-			;
-			if ("hola".equals(name)) {
-				String xml = "<good>Found</good>";
-				pw.println(xml);
-			} else {
-				pw.println("<error>Not found</error>");
+			System.out.println(name);
+			if (name.substring(0, 1).equals("1")) {
+				if ("hola".equals("hola")) {
+					String xml = "<true> Apodo libre </true>";
+					pw.println(xml);
+					int finick = 0;
+					int finame = 0;
+					int finedad = 0;
+					int fingenero = 0;
+					int fincontra = 0;
+					int j = 3;
+					int x = 0;
+					String[] genero = new String[10];
+					String[] amigo = new String [10];
+					for (int i = 2; name.length() > i; i++) {
+						if (name.substring(i, j).equals("/")) {
+							if (x == 0) {
+								finick = j;
+								x++;
+							} else if (x == 1) {
+								finame = j;
+								x++;
+							} else if (x == 2) {
+								finedad = j;
+								x++;
+							} else if (x == 3) {
+								fingenero = j;
+								x++;
+								int v = finedad + 1;
+								int z = 0;
+								int m = finedad;
+								for (int c = 0; name.substring(finedad, fingenero - 1).length() >= c; c++) {
+									if (name.substring(v - 1, v).equals(",")) {
+										genero[z] = name.substring(m, v - 1);
+										m = c + finedad + 1;
+										z++;
+									}
+									v++;
+								}
+							} else {
+								fincontra = j;
+								x++;
+								//int v = fingenero + 1;
+								//int z = 0;
+								//int m = fingenero;
+								//for (int c = 0; name.substring(fingenero, name.length()).length() >= c; c++) {
+									//if (name.substring(v - 1, v).equals(",")) {
+										//amigo[z] = name.substring(m, v - 1);
+										//m = c + fingenero + 1;
+										//z++;
+									//}
+									//v++;
+								//}
+							}
+						}
+						j++;
+					}
+					NewUser usuario = new NewUser();
+					usuario.setUserName(name.substring(2, finick-1));
+					usuario.setName(name.substring(finick, finame-1));
+					usuario.setAge(Integer.parseInt(name.substring(finame, finedad-1)));
+					usuario.setMusicalGenres(genero);
+					usuario.setPassword(name.substring(fingenero, fincontra-1));
+					usuario.setFriends(amigo);
+					JsonObject toInsert = usuario.signInUser();
+					System.out.println(toInsert.toString());
+				} else {
+					pw.println("<false> Apodo utilizado </false>");
+				}
 			}
 			client.close();
 		}
--- src/users/NewUser.java
+++ src/users/NewUser.java
@@ -23,97 +23,103 @@ public class NewUser {
 	OutputStream ots;
 	private JsonObjectBuilder userBuilder;
 	private JsonArrayBuilder musicalGenresBuilder;
-	private JsonArrayBuilder friendsBuilder;	
+	private JsonArrayBuilder friendsBuilder;
 	private JsonArrayBuilder arrayBuilder;
 	private JsonArray users;
 	private String userName;
-	
+
 	public NewUser() throws FileNotFoundException {
 		userBuilder = Json.createObjectBuilder();
 		musicalGenresBuilder = Json.createArrayBuilder();
 		friendsBuilder = Json.createArrayBuilder();
 		arrayBuilder = Json.createArrayBuilder();
 	}
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 		userBuilder.add("UserName", userName);
 	}
-	
+
 	public void setName(String name) {
 		userBuilder.add("Name", name);
 	}
-	
+
 	public void setAge(int age) {
 		userBuilder.add("Age", age);
 	}
-	
+
 	public void setMusicalGenres(String[] musicalGenres) {
-		for(String i:musicalGenres) {
-			musicalGenresBuilder.add(i);
+		for (String i : musicalGenres) {
+			if (i != null) {
+				musicalGenresBuilder.add(i);
+			}
 		}
 		userBuilder.add("MusicalGenres", musicalGenresBuilder);
 	}
-	
+
 	public void setPassword(String password) throws Exception {
 		password = encodePassword(password);
 		userBuilder.add("Password", password);
 	}
-	
+
 	public void setFriends(String[] friends) {
-		for(String i:friends) {
-			friendsBuilder.add(i);			
+		for (String i : friends) {
+			if (i != null) {
+				friendsBuilder.add(i);
+			}
 		}
 		userBuilder.add("Friends", friendsBuilder);
 	}
-	
+
 	public JsonObject signInUser() throws IOException {
 		try {
 			FileReader fileReader = new FileReader("usuarios.json");
-		
-			if(fileReader.ready()){
-				InputStream IS = new FileInputStream(new File("usuarios.json"));			
-				JsonReader reader = Json.createReader(IS);			
-				JsonArray oldArray = reader.readArray();			
-				reader.close();			
-				for(JsonValue i:oldArray) {
-					if(!i.asJsonObject().getString("UserName").equals(this.userName)) { //Autentica que el usuario no exista registrado
+
+			if (fileReader.ready()) {
+				InputStream IS = new FileInputStream(new File("usuarios.json"));
+				JsonReader reader = Json.createReader(IS);
+				JsonArray oldArray = reader.readArray();
+				reader.close();
+				for (JsonValue i : oldArray) {
+					if (!i.asJsonObject().getString("UserName").equals(this.userName)) { // Autentica que el usuario no
+																							// exista registrado
 						arrayBuilder.add(i);
-					}else {
-						System.out.println("ERROR: El usuario ya existe!");				
+					} else {
+						System.out.println("ERROR: El usuario ya existe!");
 						return null;
 					}
 				}
 			}
-			fileReader.close();		
-		}catch(Exception ex) {}
-		
-		ots =  new FileOutputStream("usuarios.json");
+			fileReader.close();
+		} catch (Exception ex) {
+		}
+
+		ots = new FileOutputStream("usuarios.json");
 		JsonObject user = userBuilder.build();
 		arrayBuilder.add(user);
-		users = arrayBuilder.build();		
+		users = arrayBuilder.build();
 		JsonWriter jsonWriter = Json.createWriter(ots);
 		jsonWriter.writeArray(users);
 		jsonWriter.close();
-		
+
 		return user;
 	}
-	
+
 	private String encodePassword(String password) throws Exception {
 		MessageDigest md = MessageDigest.getInstance("MD5");
 		md.update(password.getBytes());
-		
+
 		byte[] byteData = md.digest();
-		
+
 		StringBuffer hexaString = new StringBuffer();
-		for(int i = 0; i < byteData.length; i++) {
+		for (int i = 0; i < byteData.length; i++) {
 			String hexa = Integer.toHexString(0xff & byteData[i]);
-			if(hexa.length() == 1) {
+			if (hexa.length() == 1) {
 				hexaString.append("0");
 			}
 			hexaString.append(hexa);
 		}
-		
+
 		return hexaString.toString();
 	}
 }
